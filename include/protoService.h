ffd89808c6f7fdf89532804456aeb6ca42c89132
guzzijones <ajonen@mailcan.com>
2015-09-20 14:24:44 -0400

    first

--- include/protoService.h
+++ include/protoService.h
@@ -3,16 +3,20 @@
 
 extern "C"
 {
-#include <sys/socket.h>
+#if defined(_WIN32) 
+    #include <winsock.h>
+#elif defined(__unix__)
+    #include <sys/socket.h>
+    #include <netinet/in.h>
+    #include <netdb.h>
+    #include <arpa/inet.h>
+#endif
 #include <sys/types.h>
-#include <netinet/in.h>
-#include <netdb.h>
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <errno.h>
-#include <arpa/inet.h>
 #include <ctype.h>
 #include <stdarg.h>
 #include <string.h>
@@ -34,8 +38,8 @@ protected:
 public:
 //has static methods for send and receive
 
-static void DoWrite(const int & sock,void * data,T size);
-static void DoRead(const int & sock,void * data,T size);
+static void DoWrite(const int & sock,char * data,T size);
+static void DoRead(const int & sock,char * data,T size);
 };
 #include "../src/protoService.cpp"
 #endif
