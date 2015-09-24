34cc538647c7b6ced3b19c49b513f5b4ce09c723
guzzijones <ajonen@vm4.defaultdomain>
2015-09-24 01:20:31 +0000

    added writeread

--- include/aTodo.h
+++ include/aTodo.h
@@ -15,47 +15,11 @@ class aTodo{
    virtual ~aTodo(){};
    virtual void Do()=0;
 
-   //used for a client to send and then recieve result back
-   std::unique_ptr<aTodo>  WriteRead(std::string ip, int port){
-
-    protocolBaseServer protoConnection(ip, port);  
-    std::string objToSendStr = this->save(); //save to a string
-    protoConnection.DoWrite(objToSendStr);//send string off to server; server does some work
-    protoConnection.DoRead();  //read back in the result from the server to a string in object protoConnection
-    std::string returnedObjectStr= protoConnection.getTotalMessage(); //save the read bytes to a string
-    std::unique_ptr<aTodo> objectReturned=this->load(returnedObjectStr);
-    return objectReturned;
-   }
-
    private:
 
    friend class boost::serialization::access;
    template  <class Ar>
    void serialize(Ar &, unsigned){};//every class that inherits from aTodo must have a serialize function
-
-//save pointer to this to a string
-   std::string save(){
-         std::string out;
-         {
-            boost::iostreams::stream<boost::iostreams::back_insert_device<std::string>>os(out);
-            boost::archive::text_oarchive archive(os);
-
-            archive << this;
-         }
-         return out;
-   }
-//write aTodo pointer from string
-   std::unique_ptr<aTodo> load(std::string const &s ){
-         std::unique_ptr<aTodo> p;
-         {
-            boost::iostreams::stream<boost::iostreams::array_source> is(boost::iostreams::array_source{s.data(),s.size()});
-            boost::archive::text_iarchive archive(is);
-            archive >> p;
-
-         }
-         return std::move(p);
-
-  }
 
 
 };
