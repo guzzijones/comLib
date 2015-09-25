272ae3bb7fe5e4208e75ede378673f93912cf454
guzzijones <ajonen@vm4.defaultdomain>
2015-09-25 00:53:21 +0000

    adjusted protocolCom to be simpler for server and client usage

--- include/protocolCom.h
+++ include/protocolCom.h
@@ -8,14 +8,18 @@ class protocolCom{
 private:
 
 
+   static std::string readString(protocolBaseServer  inProtoConnection);
+   static std::unique_ptr<aTodo> load(std::string const &s );
 protected:
 
 public:
 
    static std::string save(std::unique_ptr<aTodo> & todoIn);
-   static std::unique_ptr<aTodo> load(std::string const &s );
+
    static std::unique_ptr<aTodo>  WriteRead(std::unique_ptr<aTodo> & object,std::string ip, int port);
 
+   static std::unique_ptr<aTodo> Read(protocolBaseServer in);
+   static void Write(std::unique_ptr<aTodo> & object,protocolBaseServer);
 
 
 };
--- src/protocolCom.cpp
+++ src/protocolCom.cpp
@@ -22,12 +22,35 @@ std::unique_ptr<aTodo> protocolCom::load(std::string const &s ){
 std::unique_ptr<aTodo>  protocolCom::WriteRead(std::unique_ptr<aTodo> & object,std::string ip, int port){
 
     protocolBaseServer protoConnection(ip, port);  
-    std::string objToSendStr = save(object); //save to a string
-    protoConnection.DoWrite(objToSendStr);//send string off to server; server does some work
-    protoConnection.DoRead();  //read back in the result from the server to a string in object protoConnection
-    std::string returnedObjectStr= protoConnection.getTotalMessage(); //save the read bytes to a string
-    std::unique_ptr<aTodo> objectReturned=load(returnedObjectStr);
+    Write(object,protoConnection);
+    std::unique_ptr<aTodo> objectReturned=Read(protoConnection);
     return objectReturned;
 }
 
+
+std::unique_ptr<aTodo> protocolCom::Read(protocolBaseServer inProtoBaseServer){
+
+   std::string returnedObjectStr=readString(inProtoBaseServer);
+   std::unique_ptr<aTodo> objectReturned=load(returnedObjectStr);
+   
+    return objectReturned; 
+}
+
+void protocolCom::Write(std::unique_ptr<aTodo> & object,protocolBaseServer protoConnection){
+    std::string objToSendStr = save(object); //save to a string
+    protoConnection.DoWrite(objToSendStr);//send string off to server; server does some work
+    //return bool above and return results
+}
+
+//////////////
+////private
+//////////////
+std::string protocolCom::readString(protocolBaseServer inProtoConnection){
+
+    inProtoConnection.DoRead();  //read back in the result from the server to a string in object protoConnection
+    //should return a bool here
+    //throw exception otherwise
+    std::string returnedObjectStr= inProtoConnection.getTotalMessage(); //save the read bytes to a string
+   return returnedObjectStr; 
+}
 
